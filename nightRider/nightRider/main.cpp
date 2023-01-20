a0a9786c3902e4d1fa34fc7cef2a82982b3929b5
DiYaZeN <61739248+DiYaZeN@users.noreply.github.com>
2023-01-21 02:14:10 +0530

    Add files via upload

---
+++ nightRider/nightRider/main.cpp
@@ -0,0 +1,54 @@
+/*
+ * nightRider.cpp
+ * drive 5 LEDs in Night Rider pattern efficiently. 
+ * Created: 1/20/2023 9:32:57 PM
+ * Author : minol
+ */ 
+
+#include <avr/io.h> // include I0 definitions
+#define F_CPU 16000000UL // define CPU clock speed for delay.h
+#include <util/delay.h> // include definition of _delay ms( )
+
+void moveRight();
+void moveLeft();
+
+int main(void)
+{
+	
+	DDRC =0b00011111;
+	
+    while (1) 
+    {
+
+	moveLeft();
+	moveRight();
+
+
+    }
+	
+	
+	
+
+}
+
+ void moveLeft(){
+	PORTC=0b00000001;
+	_delay_ms(50);	
+
+	for(int i=1;i<6;i++){
+		PORTC=PORTC*2;
+		_delay_ms(50);
+	}
+
+}
+
+
+ void moveRight(){
+	 	PORTC=0b00001000;
+	_delay_ms(50);
+
+	 for(int i=0;i<3;i++){
+		 PORTC=PORTC/2;
+		 _delay_ms(50);
+	 }
+}
